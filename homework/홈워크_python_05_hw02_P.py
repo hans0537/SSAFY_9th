6086d7a9f4c3219280d0bfa260b32ff3d03348e8
신성주 <hans52410537@gmail.com>
2023-01-19 15:56:47 +0900

    0119 수업내용

--- homework/홈워크_python_05_hw02_P.py
+++ homework/홈워크_python_05_hw02_P.py
@@ -13,5 +13,21 @@ print(fn_d(91))
 print(fn_d(100))
 print(fn_d(21))
 
+
+gen_nums = []
+
+def r_check(n):
+    if n == 1:
+        return
+    gen_nums.append(fn_d(n))
+    return r_check(n - 1)
+
 def is_selfnumber(n):
-    
+    r_check(n)
+
+    if n not in gen_nums:
+        print(f'{n} : 셀프 넘버 입니다.')
+    else:
+        print(f'{n} : 셀프 넘버가 아닙니다.')
+
+is_selfnumber(1000)
--- homework/홈워크_python_06_hw01_P.py
+++ homework/홈워크_python_06_hw01_P.py
@@ -6,3 +6,31 @@
 
 # 출력 예시
 # 4.0% 700.0g
+
+for _ in range(5):
+    s = input('소금물의 농도(%)와 소금물의 양(g)을 입력하십시오: ')
+
+    if 'Done' in s:
+        break
+
+    s = s.split(' ')
+
+
+
+tot_water = 0
+tot_salt = 0
+if len(s) == 1:
+    print('정보를 입력하세요')
+elif 'Done' in s and len(s) > s.index('Done') + 1:
+    print('Done 이후에 입력을 하였습니다. 다시 입력하세요')
+elif len(s) > 10:
+    print('소금물 5개만 입력 가능합니다.')
+elif 'Done' in s and len(s) % 2 == 0:
+    print('소금물의 양과 농도를 맞춰서 입력하세요')
+else:
+    for i in range(0, len(s) - 1, 2):
+        salt = int(s[i]) * int(s[i + 1]) / 100
+        tot_water += int(s[i + 1])
+        tot_salt += salt
+
+    print(f'혼합물 퍼센트 농도 : {round(tot_salt / tot_water * 100, 2)}% | 혼합물 양 : {tot_water}g')
