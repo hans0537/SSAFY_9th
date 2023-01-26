4adf555a2e589647c42f5cfdc01a46fa977fda4e
신성주 <hans52410537@gmail.com>
2023-01-26 11:48:43 +0900

    01.26 수업 내용

---
+++ SSAFY_1학기/2023-01-26/daily.py
@@ -0,0 +1,12 @@
+sample_list=[11,22,33,55,66]
+
+# 주어진 리스트의 4번째 자리(3번 인덱스)에 있는 항목을 제거하고 변수에 할당해 주세요.
+
+tmp = sample_list.pop(3)
+print(sample_list)
+print(tmp)
+my_tuple = (11, 22, 33, 44, 55, 66)
+
+# 주어진 튜플에서 44와 55의 값을 새로운 튜플에 할당해보세요
+new_tuple = my_tuple[3:5]
+print(new_tuple)
