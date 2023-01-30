48652da65df980d652d8be84b628ea12b1079a8e
신성주 <hans52410537@gmail.com>
2023-01-30 17:57:41 +0900

    01.30 과제

--- homework/홈워크_python_07_hw02_P.py
+++ homework/홈워크_python_07_hw02_P.py
@@ -1,38 +1,19 @@
-# Collatz 클래스 활용
-class Collatz:
-    # 몇번 반복하는지 저장할 클래스 변수 선언
+def collatz(num):
     cnt = 0
 
-    def __init__(self, num):
-        # 생성자를 이용하여 해당 번호를 인스턴스.num에 저장후
-        # cnt 0으로 초기화
-        Collatz.cnt = 0
-        self.num = num
-    
-    # 클래스내에 인스턴스 재귀 메서드 활용
-    def run(self):
-
-        # 종료 조건1
-        if self.num == 1:
-            return Collatz.cnt
-        # 종료 조건2
-        if Collatz.cnt > 500:
+    while num > 1:
+        if cnt > 500:
             return -1
-        
+    
         # 짝수일경우
-        if self.num % 2:
-            self.num = self.num * 3 + 1
-            Collatz.cnt += 1
-            return self.run()
+        if num % 2:
+            num = num * 3 + 1
+            cnt += 1
         # 홀수일 경우
         else:
-            self.num = self.num // 2
-            Collatz.cnt += 1
-            return self.run()
-
-# 클래스를 호출할 함수 생성     
-def collatz(n):
-    return Collatz(n).run()
+            num = num // 2
+            cnt += 1
+    return cnt
 
 print(collatz(6))  # => 8
 print(collatz(16))  # => 4
