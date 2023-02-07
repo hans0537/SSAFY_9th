fe6667790e8b34b212f065389a7a85895345a59d
sungjoo <hans52410537@gmail.com>
2023-02-07 19:11:15 +0900

    02.07 수업내용

--- homework/02_algorithm/Ladder1.py
+++ homework/02_algorithm/Ladder1.py
@@ -3,40 +3,32 @@ for tc in range(1, 11):
     ladder = [list(map(int, input().split())) for _ in range(100)]
 
     # 위에서 아래로 가는것보단 아래에서 위로 찾아 가는것이 더 효과적이다.
-    # 위 좌 우
-    dx = [-1, 0, 0]
-    dy = [0, -1, 1]
-## 좌 우 상 
+    # 좌 우 상
+    dx = [0, 0, -1]
+    dy = [-1, 1, 0]
+
     x = 99
     y = 0
     # 끝에서 2인 좌표를 찾아 좌표 설정
     for i in range(100):
         if ladder[99][i] == 2:
             y = i
+            break
 
     while True:
         if x == 0:
             break
+        
+        for i in range(3):
+            mx = x + dx[i]
+            my = y + dy[i]
 
-        if y - 1 >= 0 and ladder[x][y - 1]:
-            while True:
-                mx = x + dx[1]
-                my = y + dy[1]
-                if my >= 0 and ladder[mx][my] == 0:
-                    break
+            if mx < 0 or mx >= 100 or my < 0 or my >= 100:
+                continue
+            
+            if ladder[mx][my]:
+                ladder[mx][my] = 0
                 x = mx
                 y = my
-        elif y + 1 < 100 and ladder[x][y + 1]:
-            while True:
-                mx = x + dx[2]
-                my = y + dy[2]
-                if my < 100 and ladder[mx][my] == 0:
-                    break
-                x = mx
-                y = my
-
-        # 올라가기
-        x += dx[0]
-        y += dy[0]
-
+                break
     print(f'#{tc} {y}')
