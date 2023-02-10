13d4f8c17c31476bde37f92a0834d18bed6fb2d7
hans0537 <hans52410537@gmail.com>
2023-02-10 22:10:54 +0900

    02.10 수업내용

--- SSAFY_1학기/02_algorithm/2023-02-10/atomEnergy.py
+++ SSAFY_1학기/02_algorithm/2023-02-10/atomEnergy.py
@@ -19,8 +19,8 @@ for tc in range(1, T + 1):
 
     energy = 0
     #    상  하  좌  우   => 0.5초 에 만나는 경우도 있으니...
-    dx = [-0.5, 0.5, 0, 0]
-    dy = [0, 0, -0.5, 0.5]
+    dx = [0, 0, -0.5, 0.5]
+    dy = [0.5, -0.5, 0, 0]
 
     # 원자가 하나 남으면 종료
     while len(atom_list) > 1:
@@ -31,21 +31,64 @@ for tc in range(1, T + 1):
 
         dic = {}
         # 동일 좌표에 있는 원자들을 저장할 딕셔너리
-        for i in range(len(atom_list)):
-            a = (atom_list[i].x, atom_list[i].y)
-            if dic.get(a) is None:
-                dic[a] = [atom_list[i]]
+        for i in atom_list:
+            a = (i.x, i.y)
+            if dic.get(a):
+                dic[a].append(i)
             else:
-                dic[a].append(atom_list[i])
+                dic[a] = [i]
 
+        # 원자들을 다시 넣을 배열 초기화
         atom_list = []
         for v in dic.values():
-            print(v[0].x, v[0].y, v[0].d, v[0].k)
+            # 동일한 좌표에 있는 원자들이면 (2개 이상)
             if len(v) >= 2:
+                # 에너지 생산
                 for a in v:
                     energy += a.k
+            # 넘어가지 않은 좌표들 한에서 다시 배열에 넣기
             else:
                 if -1000 <= v[0].x <= 1000 and -1000 <= v[0].y <= 1000:
                     atom_list.append(v[0])
 
     print(f'#{tc} {energy}')
+#
+# T = int(input())
+# for tc in range(1, 1+T):
+#     n = int(input())
+#     atom = [list(map(int, input().split())) for _ in range(n)]
+#     # 격자 중간에서 만날 수도 있기 때문에 0.5초 단위로 이동한다고 가정했음
+#     d = [(0, 0.5), (0, -0.5), (-0.5, 0), (0.5, 0)]
+#
+#     result = 0
+#
+#     # 아톰 개수가 2개 이하가 되면 만날 아톰이 없으니까 종료
+#     while len(atom) >= 2:
+#         # 모든 아톰을 0.5초 단위로 이동시킨다
+#         for i in range(len(atom)):
+#             atom[i][0] = atom[i][0] + d[atom[i][2]][0]
+#             atom[i][1] = atom[i][1] + d[atom[i][2]][1]
+#
+#         # 좌표를 딕셔너리로 표현
+#         location = {}
+#         # 각 아톰들을 순회하면서 좌표: [아톰들]의 형태로 넣어준다.
+#         for a in atom:
+#             try:
+#                 location[(a[0], a[1])].append(a)
+#             except Exception:
+#                 location[(a[0], a[1])] = [a]
+#
+#         # 아톰 리스트를 초기화하고
+#         atom = []
+#         for l in location:
+#
+#             if len(location[l]) >= 2:
+#                 # 만약 같은 위치에 아톰이 여러개라면 결과값에 아톰의 에너지들을 결과값에 넣어주고
+#                 for at in location[l]:
+#                     result += at[3]
+#             else:
+#                 # 위치에 아톰이 1개라면 범위내에 있는 아톰들은 아톰 리스트에 다시 넣어준다.
+#                 if -1000 <= location[l][0][0] <= 1000 and -1000 <= location[l][0][1] <= 1000:
+#                     atom.append(location[l][0])
+#
+#     print(f'#{tc}', result)
