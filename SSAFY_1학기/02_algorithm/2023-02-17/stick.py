039e83708c4c04f521d9ca8d8dff7328e56e0a0c
sungjoo <hans52410537@gmail.com>
2023-02-20 19:07:35 +0900

    .

--- SSAFY_1학기/02_algorithm/2023-02-17/stick.py
+++ SSAFY_1학기/02_algorithm/2023-02-17/stick.py
@@ -1,15 +1,19 @@
 T = int(input())
 for tc in range(1, T + 1):
-    st = input()
-    ans = cnt = 0
-    for i in range(len(st)):
-        if st[i] == '(':    # 막대기 시작 또는 레이저
+    s = input()
+
+    ans = 0  # 정답 변수
+    cnt = 0  # 잘려 나간 막대기 수
+    for i in range(len(s)):
+        if s[i] == '(':  # 막대기 시작 또는 레이저 시작
             cnt += 1
-        else:               # ')' 바로 앞의 기호를
-            if st[i-1] == '(':  # 레이저
-                cnt -= 1
-                ans += cnt
-            else:               # 막대기의 끝
-                cnt -= 1
-                ans += 1
+        elif s[i] == ')':
+            # 바로 직전이 여는괄호면 레이저
+            if s[i - 1] == '(':
+                cnt -= 1  # 레이저 시작한거 빼고
+                ans += cnt  # 왼쪽 잘려 나간 막대기 수
+            else:
+                ans += 1  # 닫힌 거 오른쪽 잘려나간거 하나 추가
+                cnt -= 1  # 막대기 수 하나 줄이기
+
     print(f'#{tc} {ans}')
