4009cf365f77609df41a3c4c71cf7a5ff2933546
sungjoo <hans52410537@gmail.com>
2023-02-22 23:12:23 +0900

    02.22 수업 내용 복습

--- SSAFY_1학기/02_algorithm/2023-02-22/homeSecure.py
+++ SSAFY_1학기/02_algorithm/2023-02-22/homeSecure.py
@@ -11,6 +11,8 @@ def bfs(s, e):
     visited[s][e] = 1
 
     home_cnt = 0
+    # 만약 시작 위치에 집이 있으면
+    # 집이 1개 k = 1 인 경우 처리
     if city[s][e] == 1:
         home_cnt += 1
         if home_cnt * M - 1 >= 0:
@@ -24,12 +26,15 @@ def bfs(s, e):
             my = y + dy[d]
 
             if 0 <= mx < N and 0 <= my < N and not visited[mx][my]:
+                # 방문 했을때 영역안에 집이 있으면 집 개수 늘린후
                 if city[mx][my] == 1:
                     home_cnt += 1
                 q.append((mx, my))
                 visited[mx][my] = visited[x][y] + 1
                 k = visited[mx][my]
 
+        # k 영역 방향 탐색하면서 운영비용 검색하면서
+        # 손해가 아닐시 정답 변수 최신화
         if home_cnt * M - (k*k + (k-1)*(k-1)) >= 0:
             ans = max(ans, home_cnt)
 
@@ -39,6 +44,7 @@ for tc in range(1, T + 1):
     city = [list(map(int, input().split())) for _ in range(N)]
 
     ans = 0
+    # 모든 위치에서 서비스 영역 넓히기 BFS 실행
     for s in range(N):
         for e in range(N):
             bfs(s, e)
