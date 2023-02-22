ae5a5b62457463685bbce0dd1c3fa7efb1eab2a8
sungjoo <hans52410537@gmail.com>
2023-02-22 22:26:01 +0900

    02.22 추가 문제 풀이

--- SSAFY_1학기/02_algorithm/2023-02-20/getSqrt.py
+++ SSAFY_1학기/02_algorithm/2023-02-20/getSqrt.py
@@ -1,3 +1,16 @@
+def threesqrt(num):
+    dp = [0] * (num + 1)
+    for i in range(num + 1):
+        dp[i] = i ** 3
+
+    return dp
+
+
 T = int(input())
 for tc in range(1, T + 1):
     num = int(input())
+    lst = threesqrt(int(num ** (1/3)) + 1)
+    if num in lst:
+        print(f'#{tc} {lst.index(num)}')
+    else:
+        print(f'#{tc} -1')
