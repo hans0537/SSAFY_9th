66ef2fe143aba986c8fe9e5d33e79a92e5f14140
hans0537 <hans52410537@gmail.com>
2023-02-25 15:50:09 +0900

    02.24 수업내용 복습

--- SSAFY_1학기/02_algorithm/2023-02-24/waterbom.py
+++ SSAFY_1학기/02_algorithm/2023-02-24/waterbom.py
@@ -2,34 +2,34 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(si, sj):
-    q = deque()
-    visited = [[] * M for _ in range(N)]
-    q.append((si, sj))
-    visited[si][sj] = 1
-    tmp = 1e9
-    while q:
-        x, y = q.popleft()
-
-        if arr[x][y] == 'W':
-            break
+def bfs():
+    global ans
+    while water:
+        x, y = water.popleft()
 
         for d in range(4):
             mx = x + dx[d]
             my = y + dy[d]
 
-            if 0 <= mx < N and 0 <= my < M and not visited[mx][my]:
-                q.append((mx, my))
+            if 0 <= mx < N and 0 <= my < M and visited[mx][my] == -1:
+                water.append((mx, my))
                 visited[mx][my] = visited[x][y] + 1
-
-        tmp = min(tmp, visited[x][y])
+                ans += visited[mx][my]
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    arr = [list(input()) for _ in range(N)]
     ans = 0
-    for si in range(N):
-        for sj in range(M):
-            if arr[si][sj] == 'L':
-                bfs(si, sj)
+    visited = [[-1] * M for _ in range(N)]
+    # 물인 곳인 좌표들을 큐에 저장하여 모든 좌표를 동시에 bfs를 돌리기 위
+    water = deque()
+
+    for i in range(N):
+        tmp = list(input())
+        for j in range(M):
+            if tmp[j] == 'W':
+                water.append((i, j))
+                visited[i][j] = 0   # 물인곳은 거리가 0
+
+    bfs()
+    print('#{} {}'.format(tc, ans))
