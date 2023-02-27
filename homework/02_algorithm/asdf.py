d1a9ed1cc876dbf1f444561a40ea8a74cbc138e2
sungjoo <hans52410537@gmail.com>
2023-02-27 23:09:33 +0900

    02.27 수업내용

--- homework/02_algorithm/asdf.py
+++
@@ -1,2 +0,0 @@
-print(13 & 1)
-print(bin(13))
--- homework/02_algorithm/binaryCode.py
+++ homework/02_algorithm/binaryCode.py
@@ -1,42 +1,50 @@
 decode = {
-    0 : 13,
-    1 : 25,
-    2 : 19,
-    3 : 61,
-    4 : 35,
-    5 : 49,
-    6 : 47,
-    7 : 59,
-    8 : 55,
-    9 : 11
+    '0001101': '0',
+    '0011001': '1',
+    '0010011': '2',
+    '0111101': '3',
+    '0100011': '4',
+    '0110001': '5',
+    '0101111': '6',
+    '0111011': '7',
+    '0110111': '8',
+    '0001011': '9'
 }
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    code = [list(map(int, list(input()))) for _ in range(N)]
+    code = [list(input()) for _ in range(N)]
 
     code_num = ''
-    check = False
-    for row in code:
-        if check:
-            break
 
-        if row.count(1) == 0:
+    for row in code:
+
+        # 0 인것들은 무시
+        if row.count('1') == 0:
             continue
 
-        i = 0
-        while i + 7 < M:
-            tmp = row[i : i + 7]
-            for j in tmp:
-                for d in range(10):
-                    if decode[d] & (2**j) != 0:
-                        code_num += str(d)
-                        i += 7
-                        check = True
-                        break
+        idx = 0
+        # 뒤에서부터 확인해서 처음 1 인 idx값 가져오기
+        for i in range(M - 1, -1, -1):
+            if row[i] == '1':
+                idx = i
+                break
 
-    print(code_num)
+        while idx > 0:
+            if ''.join(row[idx - 6:idx + 1]) in decode:
+                code_num += decode.get(''.join(row[idx - 6:idx + 1]))
+                idx -= 7
+            else:
+                break
+        break
 
+    cnt1 = cnt2 = 0
+    # 거꾸로 저장했기 때문에 반대로 저장
+    for i in range(len(code_num)):
+        if i % 2:
+            cnt1 += int(code_num[i])
+        else:
+            cnt2 += int(code_num[i])
 
-    # print(code)
+    print('#{} {}'.format(tc, 0 if (cnt1*3 + cnt2) % 10 else (cnt1 + cnt2)))
