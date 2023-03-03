fca99276ccde0142317c72366626b7601f15a35b
hans52410537 <hans52410537@gmail.com>
2023-03-03 17:53:15 +0900

    IM 대비 문제풀이

--- SSAFY_1학기/02_algorithm/2023-03-03/poping.py
+++ SSAFY_1학기/02_algorithm/2023-03-03/poping.py
@@ -1,7 +1,28 @@
 from collections import deque
 dx = [-1, -1, -1, 0, 1, 1, 1, 0]
 dy = [-1, 0, 1, 1, 1, 0, -1, -1]
-def bfs(x, y):
+def bfs(s, e):
+    global cnt
+    q = deque()
+    q.append((s, e))
+    visited[s][e] = 1
+    cnt += 1
+
+      while q:
+        x, y = q.popleft()
+
+        tmp = 0
+        for d in range(8):
+            mx = x + dx[d]
+            my = y + dy[d]
+
+            if 0 <= mx < N and 0 <= my < N and not visited[mx][my]:
+                if arr[mx][my] == '*':
+                    tmp += 1
+                else:
+                    visited[mx][my] = 1
+                    q.append((mx, my))
+        arr[x][y] = tmp
 
 
 T = int(input())
@@ -13,11 +34,11 @@ for tc in range(1, T + 1):
     lst = deque()
     for i in range(N):
         for j in range(N):
-            if arr[i][j] == '*':
-                visited[i][j] = -1
-            elif arr[i][j] == '.':
+            # if arr[i][j] == '*':
+            #     visited[i][j] = -1
+            if arr[i][j] == '.':
                 lst.append((i, j))
 
     cnt = 0
-    s, e = lst.popleft(0)
+    s, e = lst.popleft()
     bfs(s, e)
