ac8ca51e698fe72267e2620b6bced03701e21475
hans52410537 <hans52410537@gmail.com>
2023-03-14 14:39:14 +0900

    03.14 django

--- SSAFY_1학기/05_django/0314/my_pjt/my_pjt/my_pjt/asgi.py
+++ SSAFY_1학기/05_django/0314/my_pjt/my_pjt/my_pjt/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for first_pjt project.
+ASGI config for my_pjt project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_pjt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_pjt.settings')
 
 application = get_asgi_application()
