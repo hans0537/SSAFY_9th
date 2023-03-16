1c0a2672562d5893795f46f4c10f21b9e48fdefa
신성주 <hans52410537@gmail.com>
2023-03-16 17:56:06 +0900

    03.16 django

--- SSAFY_1학기/05_django/0316/prac/mypjt/articles/models.py
+++ SSAFY_1학기/05_django/0316/prac/mypjt/articles/models.py
@@ -6,3 +6,6 @@ class Article(models.Model):    # models 상속
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.created_at.month}/{self.created_at.day}에 생성된 {self.id}번글 - {self.title} : {self.content}'
