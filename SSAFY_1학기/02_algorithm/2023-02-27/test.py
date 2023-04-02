526cae08e9330fa09fb8e826dbf21686b8a053dc
sungjoo <hans52410537@gmail.com>
2023-04-03 00:11:44 +0900

    .

--- SSAFY_1학기/02_algorithm/2023-02-27/test.py
+++ SSAFY_1학기/02_algorithm/2023-02-27/test.py
@@ -8,8 +8,13 @@ def solution(n):
         for j in range(7):
             if i - j < 0:
                 break
-            print(i-j, 1 << i - j)
-            # bin += '1' if x & (1 << i - j)
+            bin += '1' if x & (1 << i - j) else '0'
+
+        print(bin, end=' ')
+        dec = int(bin, 2)
+        ans += str(dec) + ' '
+    print()
+    print(ans)
 
 h1 = '0F97A3'
 h2 = '01D06079861D79F99F'
